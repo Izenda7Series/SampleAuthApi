73fcd996927af2b74e7f221e72140933cd958387
max_butkov <max.butkov@izenda.com>
2020-12-15 15:48:06 -0500

    CoreApiSample for .Net Core 2.2 is added to the main CoreApiSample. Mostly for the needs of the installer and also for anybody who might need the authentication sample working under Core 2.2

---
+++ CoreApiSample/Core2.2ApiSample/Program2.2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using SampleAuthAPI.CoreApiSample.Shared;
+
+namespace SampleAuthAPI.CoreApiSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Running under core 2.2");
+            CreateWebHostBuilder(args, Utilities.GetStartupUrls(args)).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args, string urls) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>().UseUrls(urls);
+    }
+}
